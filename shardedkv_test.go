2713e307d13e46c4a9e96b8843ae7fb868fbfef2
Damian Gryski <damian@gryski.com>
2014-03-02 14:13:27 +0100

    improved test coverage for migration code.  still ugly though.

--- shardedkv_test.go
+++ shardedkv_test.go
@@ -1,10 +1,11 @@
 package shardedkv
 
 import (
-	kc "github.com/dgryski/go-shardedkv/choosers/ketama"
-	st "github.com/dgryski/go-shardedkv/storage/memory"
 	"strconv"
 	"testing"
+
+	kc "github.com/dgryski/go-shardedkv/choosers/ketama"
+	st "github.com/dgryski/go-shardedkv/storage/memory"
 )
 
 func TestShardedkv(t *testing.T) {
@@ -38,4 +39,92 @@ func TestShardedkv(t *testing.T) {
 		}
 	}
 
+	var migrationBuckets []string
+
+	for i := nShards; i < nShards*2; i++ {
+		label := "test_shard" + strconv.Itoa(i)
+		migrationBuckets = append(migrationBuckets, label)
+		backend := st.New()
+		shards = append(shards, Shard{Name: label, Backend: backend})
+		kv.AddShard(label, backend)
+	}
+
+	migration := kc.New()
+	migration.SetBuckets(migrationBuckets)
+
+	kv.BeginMigration(migration)
+
+	// make sure requesting still works
+	for i := 0; i < nElements; i++ {
+		k := "test" + strconv.Itoa(i)
+
+		v, ok, err := kv.Get(k)
+		if ok != true {
+			t.Errorf("failed  to get key: %s\n", err)
+		}
+
+		if string(v) != "value"+strconv.Itoa(i) {
+			t.Errorf("failed to get a valid value: %s != \"value%d\"\n", v, i)
+		}
+	}
+
+	// make sure setting still works
+	for i := 0; i < nElements; i++ {
+		kv.Set("test"+strconv.Itoa(i), []byte("value"+strconv.Itoa(i)))
+	}
+
+	for i := 0; i < nElements; i++ {
+		k := "test" + strconv.Itoa(i)
+
+		v, ok, err := kv.Get(k)
+		if ok != true {
+			t.Errorf("failed  to get key: %s\n", err)
+		}
+
+		if string(v) != "value"+strconv.Itoa(i) {
+			t.Errorf("failed to get a valid value: %s != \"value%d\"\n", v, i)
+		}
+	}
+
+	// and that deleting removes from both during a migration
+	for i := 0; i < nElements; i++ {
+		kv.Delete("test" + strconv.Itoa(i))
+	}
+
+	for i := 0; i < nElements; i++ {
+		k := "test" + strconv.Itoa(i)
+
+		_, ok, _ := kv.Get(k)
+		if ok {
+			t.Errorf("got a key that shouldn't have been there")
+		}
+	}
+
+	// set the keys again
+	for i := 0; i < nElements; i++ {
+		kv.Set("test"+strconv.Itoa(i), []byte("value"+strconv.Itoa(i)))
+	}
+
+	// end the migration[
+	kv.EndMigration()
+
+	// delete the old shards
+	for i := 0; i < nShards; i++ {
+		label := "test_shard" + strconv.Itoa(i)
+		kv.DeleteShard(label)
+	}
+
+	// and make sure we can still get to the keys
+	for i := 0; i < nElements; i++ {
+		k := "test" + strconv.Itoa(i)
+
+		v, ok, err := kv.Get(k)
+		if ok != true {
+			t.Errorf("failed  to get key: %s\n", err)
+		}
+
+		if string(v) != "value"+strconv.Itoa(i) {
+			t.Errorf("failed to get a valid value: %s != \"value%d\"\n", v, i)
+		}
+	}
 }
